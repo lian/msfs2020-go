9e8bf05a27cda21fc64ddbf9c7a249deeeb5d222
Julian Langschaedel <meta.rb@gmail.com>
2020-08-28 05:17:34 +0200

    strip debug info from builds

--- build-vfrmap.sh
+++ build-vfrmap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-go generate github.com/lian/msfs2020-go/simconnect
-go generate github.com/lian/msfs2020-go/vfrmap
-go generate github.com/lian/msfs2020-go/vfrmap/html/leafletjs
+[ -x "$(command -v go-bindata)" ] && go generate github.com/lian/msfs2020-go/simconnect
+[ -x "$(command -v go-bindata)" ] && go generate github.com/lian/msfs2020-go/vfrmap
+[ -x "$(command -v go-bindata)" ] && go generate github.com/lian/msfs2020-go/vfrmap/html/leafletjs
 
 build_time=$(date -u +'%Y-%m-%d_%T')
 set +e
 build_version=$(git describe --tags)
 set -e
 
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.buildVersion=$build_version -X main.buildTime=$build_time" -v github.com/lian/msfs2020-go/vfrmap
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X main.buildVersion=$build_version -X main.buildTime=$build_time" -v github.com/lian/msfs2020-go/vfrmap
